33e9d29ff21665b6e5073146c3fec04e66e9e112
Thevidu Sathsara Gamage <thevidu2007627@gmail.com>
2025-03-04 23:55:21 +0530

    send error code using json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import validator from "email-validator";
 import env from "dotenv";
 env.config();
 
@@ -14,7 +15,7 @@ const db = new pg.Client({
   database: process.env.DB_NAME,
 });
 
-db.connect()
+db.connect();
 
 const app = express();
 
@@ -24,7 +25,7 @@ app.get("/", (req, res) => {
   res.send("Hello Princess");
 });
 
-app.post("/customers/register", async(req, res) => {
+app.post("/customers/register", async (req, res) => {
   const {
     name,
     address,
@@ -38,66 +39,107 @@ app.post("/customers/register", async(req, res) => {
     cvv,
   } = req.body;
 
-let insertResult
+  let insertResult;
+
+  if (
+    !name ||
+    !address ||
+    !email ||
+    !dateOfBirth ||
+    !gender ||
+    !age ||
+    !cardHolderName ||
+    !cardNumber ||
+    !expireDate ||
+    !cvv
+  ) {
+    res.status(400);
+    res.json({
+      error: "Missing required fields",
+      code: "400",
+    });
+    return;
+  }
+  if (cardNumber.length !== 12) {
+    res.status(422);
+    res.json({
+      error: "Card number should be 12 digits",
+      code: "422",
+    });
+    return;
+  }
+  if (!validator.validate(email)) {
+    res.status(422);
+    res.json({
+      error: "Invalid email address",
+      code: "422",
+    });
+    return;
+  }
 
   try {
-
     insertResult = await db.query(
-        "INSERT INTO customerRegister (name, address, email, dob, gender, age, cardholdername, cardnumber, expiredate , cvv) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id, name",
-        [
-          name,
-          address,
-          email,
-          dateOfBirth,
-          gender,
-          age,
-          cardHolderName,
-          cardNumber,
-          expireDate,
-          cvv,
-        ]
-      );
-      
+      "INSERT INTO customerRegister (name, address, email, dob, gender, age, cardholdername, cardnumber, expiredate , cvv) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id, name",
+      [
+        name,
+        address,
+        email,
+        dateOfBirth,
+        gender,
+        age,
+        cardHolderName,
+        cardNumber,
+        expireDate,
+        cvv,
+      ]
+    );
+
     console.log("Inserted customer data: ", insertResult.rows[0]);
     res.status(201);
     res.send({
       message: `Customer ${insertResult.rows[0].name} registered successfully`,
       customertId: insertResult.rows[0].id,
     });
-} catch (error) {
-    console.log("Error while inserting data: ", error);
+  } catch (error) {
+    console.log(
+      "Error while inserting data: ",
+      error["detail"],
+      "in table",
+      error["table"]
+    );
     res.status(400);
-    res.send("Bad Request");
-}
-// console.log(
-    //   "Received customer registration request:",
-    //   "Name:",
-    //   name,
-    //   "Address:",
-    //   address,
-    //   "Email:",
-    //   email,
-    //   "Date of Birth:",
-    //   dateOfBirth,
-    //   "Gender:",
-    //   gender,
-    //   "Age:",
-    //   age,
-    //   "Card Holder Name:",
-    //   cardHolderName,
-    //   "Card Number:",
-    //   cardNumber,
-    //   "Expire Date:",
-    //   expireDate,
-    //   "CVV:",
-    //   cvv
-    // );
+    res.send("Bad Request - Email already exists");
+    return;
+  }
+  // console.log(
+  //   "Received customer registration request:",
+  //   "Name:",
+  //   name,
+  //   "Address:",
+  //   address,
+  //   "Email:",
+  //   email,
+  //   "Date of Birth:",
+  //   dateOfBirth,
+  //   "Gender:",
+  //   gender,
+  //   "Age:",
+  //   age,
+  //   "Card Holder Name:",
+  //   cardHolderName,
+  //   "Card Number:",
+  //   cardNumber,
+  //   "Expire Date:",
+  //   expireDate,
+  //   "CVV:",
+  //   cvv
+  // );
 });
 
-app.get("/customers", async(req, res) => {
-    const constomerData = await db.query("SELECT * FROM customerRegister");
-    res.send(constomerData.rows);
-})
+app.get("/customers", async (req, res) => {
+  const constomerData = await db.query("SELECT * FROM customerRegister");
+  res.send(constomerData.rows);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
